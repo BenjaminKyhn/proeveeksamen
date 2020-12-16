4db4e9b111746a722b967b45339f1021702c93cf
Benjamin Kyhn <benkyhn@hotmail.com>
2020-12-16 13:03:08 +0100

    Tilføjet en route, der skaber et nyt JSON-objekt og tilføjer det til det hidtidige JSON

--- routes/sensors.js
+++ routes/sensors.js
@@ -72,7 +72,32 @@ router.get('/', function(req, res, next) {
 /* GET sensordata for the specified sensor ID */
 router.get('/:sensor_id', function(req, res, next) {
   let sensor = req.params.sensor_id;
+  console.log(req.params.sensor_id)
   res.send(json[sensor]);
+});
+
+router.post('/add-sensor', function(req, res, next) {
+  console.log(req.body)
+  console.log(req.body.id)
+
+  const newSensor = {
+    "id": req.body.id,
+    "bygning": req.body.bygning,
+    "lokale": req.body.lokale,
+    "temperatur": req.body.temperatur,
+    "luftfugtighed": req.body.luftfugtighed,
+    "co2": req.body.co2,
+    "luftkvalitet": req.body.luftkvalitet,
+    "lysniveau": req.body.lysniveau,
+    "lysfarve": req.body.lysfarve,
+    "lydniveau": req.body.lydniveau
+  }
+
+  json.push(newSensor);
+
+  res.send(json);
+
+  //TODO: tilføj persistence
 });
 
 module.exports = router;
